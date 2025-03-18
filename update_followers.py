f6d57b6450ff33f522dae36c0c18771bb02d8f0b
Sendcloud-IT-Support <91195504+Sendcloud-IT-Support@users.noreply.github.com>
2025-03-18 15:30:49 +0100

    Update update_followers.py

--- update_followers.py
+++ update_followers.py
@@ -13,7 +13,6 @@ def get_followers():
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
     chrome_options.add_argument("window-size=1920,1080")  # Ensure full page loads
 
     # Use different ChromeDriver paths based on the environment
